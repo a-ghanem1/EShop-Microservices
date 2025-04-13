7b0c801217e68086e135efe02123a18f1ebdaf00
Abdelrahman Ghanem <a.ghanem2244@gmail.com>
2025-04-13 22:30:18 +0300

    feat(catalog): add validators to products feature

--- src/Services/Catalog.API/Features/Products/CreateProduct/CreateProductCommandHandler.cs
+++ src/Services/Catalog.API/Features/Products/CreateProduct/CreateProductCommandHandler.cs
@@ -3,13 +3,18 @@ namespace Catalog.API.Features.Products.CreateProduct;
 internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResponse>
 {
    private readonly IDocumentSession _session;
-   public CreateProductCommandHandler(IDocumentSession session)
+   private readonly ILogger<CreateProductCommandHandler> _logger;
+
+   public CreateProductCommandHandler(ILogger<CreateProductCommandHandler> logger, IDocumentSession session)
    {
+      _logger = logger;
       _session = session;
    }
 
    public async Task<CreateProductResponse> Handle(CreateProductCommand command, CancellationToken cancellationToken)
    {
+      _logger.LogInformation("Handling CreateProductCommandHandler: {@Request}", command);
+
       var product = command.Adapt<Product>();
 
       _session.Store(product);
