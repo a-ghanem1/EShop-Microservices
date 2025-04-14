d504607200bfdd79aec1ceb9789a1b71474b22f8
Abdelrahman Ghanem <a.ghanem2244@gmail.com>
2025-04-14 22:31:48 +0300

    feat(catalog): add health checks

--- src/Services/Catalog.API/Program.cs
+++ src/Services/Catalog.API/Program.cs
@@ -1,8 +1,11 @@
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
 var assembly = typeof(Program).Assembly;
-builder.Services.AddMediatR(config => 
+builder.Services.AddMediatR(config =>
 {
    config.RegisterServicesFromAssembly(assembly);
    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
@@ -15,7 +18,7 @@ builder.Services.AddCarter();
 
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
-builder.Services.AddMarten(opts => 
+builder.Services.AddMarten(opts =>
 {
    opts.Connection(builder.Configuration.GetConnectionString("DefaultConnection"));
 }).UseLightweightSessions();
@@ -23,11 +26,19 @@ builder.Services.AddMarten(opts =>
 if (builder.Environment.IsDevelopment())
    builder.Services.InitializeMartenWith<InitialDataSeeder>();
 
+builder.Services.AddHealthChecks()
+   .AddNpgSql(builder.Configuration.GetConnectionString("DefaultConnection"));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
 app.MapCarter();
 
-app.UseExceptionHandler(options => {});
+app.UseExceptionHandler(options => { });
+
+app.UseHealthChecks("/health", new HealthCheckOptions
+{
+   ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
 
 app.Run();
