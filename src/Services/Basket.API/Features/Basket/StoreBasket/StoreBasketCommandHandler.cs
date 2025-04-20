fbf387d841462bbbafef3e19e3afd7fca0733e24
Abdelrahman Ghanem <a.ghanem2244@gmail.com>
2025-04-20 15:07:48 +0300

    feat(basket): implement deduct discount using grpc with discount.grpc project

--- src/Services/Basket.API/Features/Basket/StoreBasket/StoreBasketCommandHandler.cs
+++ src/Services/Basket.API/Features/Basket/StoreBasket/StoreBasketCommandHandler.cs
@@ -1,23 +1,37 @@
 
+using Discount.Grpc;
+
 namespace Basket.API.Features.Basket.StoreBasket;
 
 public class StoreBasketCommandHandler : ICommandHandler<StoreBasketCommand, StoreBasketResponse>
 {
    private readonly IBasketRepository _basketRepository;
-   public StoreBasketCommandHandler(IBasketRepository basketRepository)
+   private readonly DiscountProtoService.DiscountProtoServiceClient _discountProto;
+
+   public StoreBasketCommandHandler(IBasketRepository basketRepository, DiscountProtoService.DiscountProtoServiceClient discountProto)
    {
       _basketRepository = basketRepository;
+      _discountProto = discountProto;
    }
 
    public async Task<StoreBasketResponse> Handle(StoreBasketCommand request, CancellationToken cancellationToken)
    {
-      //TODO: store basket in database (use Marten upsert - if exist = update, if not exist = insert)
-      //TODO: update cache
+      await DeductDiscount(request.Cart, cancellationToken);
+
       await _basketRepository.StoreBasket(request.Cart, cancellationToken);
 
       return new StoreBasketResponse
       {
          UserName = request.Cart.UserName
       };
+   }
+
+   private async Task DeductDiscount(ShoppingCart cart, CancellationToken cancellationToken)
+   {
+      foreach (var item in cart.Items)
+      {
+         var coupon = await _discountProto.GetDiscountAsync(new GetDiscountRequest { ProductName = item.ProductName }, cancellationToken: cancellationToken);
+         item.Price -= coupon.Amount;
+      }
    }
 }
