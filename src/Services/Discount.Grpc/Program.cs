ba3e9b3c79f9f31a176af3f0affcaf08f332bf24
Abdelrahman Ghanem <a.ghanem2244@gmail.com>
2025-04-20 13:40:28 +0300

    feat(discount): add sqlite and DiscountContext with migrations and seeding

--- src/Services/Discount.Grpc/Program.cs
+++ src/Services/Discount.Grpc/Program.cs
@@ -1,8 +1,16 @@
+using Discount.Grpc.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
+builder.Services.AddDbContext<DiscountDbContext>(opts =>
+   opts.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseMigration();
 
 app.Run();
