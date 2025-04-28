d4e0e12d7c4b0d37e592a67ec8b73718ffb87463
Abdelrahman Ghanem <a.ghanem2244@gmail.com>
2025-04-28 23:06:35 +0300

    fix(ordering.infrastructure): create separate Address instances for ShippingAddress and BillingAddress in seed data

--- src/Services/Ordering/Ordering.Infrastructure/Extensions/InitialData.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Extensions/InitialData.cs
@@ -20,29 +20,32 @@ internal class InitialData
     {
         get
         {
-            var address1 = Address.Of("ghanem", "ghanem", "ghanem@gmail.com", "Bahcelievler No:4", "Turkey", "Istanbul", "38050");
-            var address2 = Address.Of("john", "doe", "john@gmail.com", "Broadway No:1", "England", "Nottingham", "08050");
+            var shippingAddress1 = Address.Of("ghanem", "ghanem", "ghanem@gmail.com", "Bahcelievler No:4", "Turkey", "Istanbul", "38050");
+            var billingAddress1 = Address.Of("ghanem", "ghanem", "ghanem@gmail.com", "Bahcelievler No:4", "Turkey", "Istanbul", "38050");
+
+            var shippingAddress2 = Address.Of("john", "doe", "john@gmail.com", "Broadway No:1", "England", "Nottingham", "08050");
+            var billingAddress2 = Address.Of("john", "doe", "john@gmail.com", "Broadway No:1", "England", "Nottingham", "08050");
 
             var payment1 = Payment.Of("ghanem", "5555555555554444", "12/28", "355", 1);
             var payment2 = Payment.Of("john", "8885555555554444", "06/30", "222", 2);
 
             var order1 = Order.Create(
-                            OrderId.Of(Guid.NewGuid()),
-                            CustomerId.Of(new Guid("58c49479-ec65-4de2-86e7-033c546291aa")),
-                            OrderName.Of("ORD_1"),
-                            shippingAddress: address1,
-                            billingAddress: address1,
-                            payment1);
+                OrderId.Of(Guid.NewGuid()),
+                CustomerId.Of(new Guid("58c49479-ec65-4de2-86e7-033c546291aa")),
+                OrderName.Of("ORD_1"),
+                shippingAddress: shippingAddress1,
+                billingAddress: billingAddress1,
+                payment1);
             order1.Add(ProductId.Of(new Guid("5334c996-8457-4cf0-815c-ed2b77c4ff61")), 2, 500);
             order1.Add(ProductId.Of(new Guid("c67d6323-e8b1-4bdf-9a75-b0d0d2e7e914")), 1, 400);
 
             var order2 = Order.Create(
-                            OrderId.Of(Guid.NewGuid()),
-                            CustomerId.Of(new Guid("189dc8dc-990f-48e0-a37b-e6f2b60b9d7d")),
-                            OrderName.Of("ORD_2"),
-                            shippingAddress: address2,
-                            billingAddress: address2,
-                            payment2);
+                OrderId.Of(Guid.NewGuid()),
+                CustomerId.Of(new Guid("189dc8dc-990f-48e0-a37b-e6f2b60b9d7d")),
+                OrderName.Of("ORD_2"),
+                shippingAddress: shippingAddress2,
+                billingAddress: billingAddress2,
+                payment2);
             order2.Add(ProductId.Of(new Guid("4f136e9f-ff8c-4c1f-9a33-d12f689bdab8")), 1, 650);
             order2.Add(ProductId.Of(new Guid("6ec1297b-ec0a-4aa1-be25-6726e3b51a27")), 2, 450);
 
