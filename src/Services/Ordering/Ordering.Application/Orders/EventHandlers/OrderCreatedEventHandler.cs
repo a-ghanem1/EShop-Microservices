fa569d5cebd4a34e2dad01714b6cb8b26e445646
Abdelrahman Ghanem <a.ghanem2244@gmail.com>
2025-04-30 15:10:14 +0300

    feat(ordering.app): add order event handlers

---
+++ src/Services/Ordering/Ordering.Application/Orders/EventHandlers/OrderCreatedEventHandler.cs
@@ -0,0 +1,18 @@
+namespace Ordering.Application.Orders.EventHandlers
+{
+    public class OrderCreatedEventHandler : INotificationHandler<OrderCreatedEvent>
+    {
+        private readonly ILogger<OrderCreatedEventHandler> _logger;
+
+        public OrderCreatedEventHandler(ILogger<OrderCreatedEventHandler> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task Handle(OrderCreatedEvent notification, CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Domain Event handled: {DomainEvent}", notification.GetType().Name);
+            return Task.CompletedTask;
+        }
+    }
+}
